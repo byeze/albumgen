514a0d1ac747e1640b7a6dd347c3d60aa1936f03
Ezequiel <ezecapo20111@gmail.com>
2020-03-24 03:14:26 -0300

    Updates!

--- app.js
+++ app.js
@@ -1,19 +1,84 @@
 var opacidad = 1;
 var grayscale = 0;
 var title_size = 65;
+var preview_title_size = 17;
 
 $('.dl').click(function () {
-    html2canvas(document.querySelector("#canvas"), {
-        allowTaint: true,
-        useCORS: true
-    }).then(canvas => {
-        var a = document.createElement('a');
-        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-        a.download = $('.album-title').val().split(' ').join('-') + '.jpg';
-        a.click();
-        a.remove();
-    });
+
+    if ($(window).width() < 450) {
+        $('.album-art').css('width', '600px')
+        $('.album-art').css('height', '600px')
+        $('.canvas-image').css('width', '600px')
+        $('.canvas-image').css('height', '600px')
+        $('.canvas-opacity').css('width', '600px')
+        $('.canvas-opacity').css('height', '600px')
+
+        $('body, html').animate({
+            scrollTop: $("#app").offset().top
+        }, 1);
+
+        $('.album-title').removeClass('atr');
+        $('.album-title').css('font-size', title_size);
+
+        setTimeout(function () {
+            html2canvas(document.querySelector(".album-art"), {
+                allowTaint: true,
+                useCORS: true,
+                width: 600,
+                height: 600
+            }).then(canvas => {
+                var a = document.createElement('a');
+                a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+                a.download = $('.album-title').val().split(' ').join('-') + '.jpg';
+                a.click();
+                a.remove();
+            });
+
+            $('.album-art').css('width', '270px')
+            $('.album-art').css('height', '270px')
+            $('.canvas-image').css('width', '270px')
+            $('.canvas-image').css('height', '270px')
+            $('.canvas-opacity').css('width', '270px')
+            $('.canvas-opacity').css('height', '270px')
+            $('.album-title').addClass('atr');
+            $('.album-title').css('font-size', preview_title_size);
+        }, 300);
+    } else {
+        html2canvas(document.querySelector(".album-art"), {
+            allowTaint: true,
+            useCORS: true
+        }).then(canvas => {
+            var a = document.createElement('a');
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = $('.album-title').val().split(' ').join('-') + '.jpg';
+            a.click();
+            a.remove();
+        });
+    }
+
 });
+
+window.addEventListener('resize', function () {
+    if ($(window).width() > 450) {
+        $('.album-art').css('width', '600px')
+        $('.album-art').css('height', '600px')
+        $('.canvas-image').css('width', '600px')
+        $('.canvas-image').css('height', '600px')
+        $('.canvas-opacity').css('width', '600px')
+        $('.canvas-opacity').css('height', '600px')
+        $('.album-title').removeClass('atr');
+        $('.album-title').css('font-size', title_size);
+    } else {
+        $('.album-art').css('width', '270px')
+        $('.album-art').css('height', '270px')
+        $('.canvas-image').css('width', '270px')
+        $('.canvas-image').css('height', '270px')
+        $('.canvas-opacity').css('width', '270px')
+        $('.canvas-opacity').css('height', '270px')
+        $('.album-title').addClass('atr');
+        $('.album-title').css('font-size', preview_title_size);
+    }
+})
 
 $('#bgurl').bind('input', function () {
     $('.canvas-image').css('background-image', 'url(' + $(this).val() + ')');
@@ -54,8 +119,9 @@ function addOrChangeFilter(filter_name, filter_value) {
     }
 }
 
-$('#oobv').bind('input', function () {
+$('#oobv').change(function () {
     //addOrChangeFilter('opacity', $(this).val());
+    $('#oobv_value').html($('#oobv').val())
     $('.canvas-opacity').css('background-color', 'rgba(0, 0, 0, ' + $(this).val() + ')');
 });
 
@@ -109,14 +175,32 @@ $("#upperAuthor").change(function () {
     }
 });
 
-$('#moreTitleSize').click(function() {
-    // 5 in 5 scale
-    title_size += 5;
-    $('.album-title').css('font-size', title_size);
+$('#moreTitleSize').click(function () {
+    // If mobile
+    if ($('.album-art').css('width') == '270px') {
+        // Se suma 48px al tamaño preview
+        preview_title_size += 5;
+        title_size = preview_title_size + 48;
+        $('.album-title').css('font-size', preview_title_size + 'px');
+        console.log(preview_title_size);
+    } else {
+        // 5 in 5 scale
+        title_size += 5;
+        $('.album-title').css('font-size', title_size + 'px');
+    }
 });
 
-$('#minusTitleSize').click(function() {
-    // 5 in 5 scale
-    title_size -= 5;
-    $('.album-title').css('font-size', title_size);
+$('#minusTitleSize').click(function () {
+    // If mobile
+    if ($('.album-art').css('width') == '270px') {
+        // Se resta 48px al tamaño preview
+        console.log(preview_title_size);
+        preview_title_size -= 5;
+        title_size = 48 - preview_title_size;
+        $('.album-title').css('font-size', preview_title_size + 'px');
+    } else {
+        // 5 in 5 scale
+        title_size += 5;
+        $('.album-title').css('font-size', title_size + 'px');
+    }
 });
